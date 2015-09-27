a3ef511d03c3d78446502a0e62cdb5e3c53f3348
alexsoble <asoble@gmail.com>
2015-09-27 09:24:03 -0700

    Configure passwordless postgres for testing

--- test_web_server_with_psql.sh
+++ test_web_server_with_psql.sh
@@ -4,6 +4,20 @@
 sudo -u postgres createuser ubuntu -s
 createdb student_insights_production
 
+# Configure passwordless postgres for testing
+echo "# Database administrative login by Unix domain socket
+local   all             all                                     trust
+
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     trust
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            trust
+# IPv6 local connections:
+host    all             all             ::1/128                 trust
+" | sudo tee /etc/postgresql/9.3/main/pg_hba.conf
+
 echo "production:
   adapter: postgresql
   encoding: unicode
