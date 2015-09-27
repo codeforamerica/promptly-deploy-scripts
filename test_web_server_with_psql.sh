a5ff315fc9353068404ec9255f4bb08dc19f73cb
alexsoble <asoble@gmail.com>
2015-09-27 09:43:50 -0700

    Oops

--- test_web_server_with_psql.sh
+++ test_web_server_with_psql.sh
@@ -10,7 +10,7 @@ local   all             all                                     trust
 
 # TYPE  DATABASE        USER            ADDRESS                 METHOD
 
-# "local" is for Unix domain socket connections only
+# 'local' is for Unix domain socket connections only
 local   all             all                                     trust
 # IPv4 local connections:
 host    all             all             127.0.0.1/32            trust
